a765fbc8b990e9b77ebc517c16baf9f57b5e547d
jisu youn <jisu.youn@nhn.com>
2014-07-21 16:45:12 +0900

    code review

--- 2주차/consolePrint.js
+++ 2주차/consolePrint.js
@@ -10,6 +10,9 @@ window.addEventListener('load', function(){
 		}
 	};
 
+        //delegation 방법으로 구현했네. 잘했다.
+        //원래 출제 의도는 아래 코드가 registerEvents 안에 들어가는 거였거든. 
+        //그렇게 되면 THIS가 좀 달라지고, 그 문제를 BIND로 해결하면 됨.
 	document.querySelector('ul').addEventListener('click', function(e){obj.registerEvents(e)});
 
 });
